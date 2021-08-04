2d0c32083f8d6aceafc1270159baca7369f54b0c
corpa00114 <adrianmarino@gmail.com>
2021-08-04 14:20:54 -0300

    feat: filter very high correlated variables only

--- lib/correlation.R
+++ lib/correlation.R
@@ -9,6 +9,6 @@ p_load(caret)
 #
 #
 find_high_correlated_columns <- function(df, cutoff=0.8) {
-  columns <- findCorrelation(cor(df), cutoff=cutoff)
+  columns <- findCorrelation(cor(df), cutoff=cutoff, verbose = T)
   names(df)[columns]
 }
